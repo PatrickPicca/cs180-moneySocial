3926175fb3ad360c390b4a258ece9b5447fcdf1f
Bilal <bnasi004@ucr.edu>
2023-03-09 21:52:09 -0800

    create groups work

--- moneysocial/app/screens/CreateGroupScreen.js
+++ moneysocial/app/screens/CreateGroupScreen.js
@@ -1,52 +1,113 @@
-import React, { useState } from 'react'
-import { StyleSheet, Text, View, TouchableOpacity, Keyboard } from 'react-native'
-import { TextInput } from 'react-native'
-import AntDesign from 'react-native-vector-icons/AntDesign';
-import { useNavigation } from '@react-navigation/native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, SafeAreaView, TextInput, TouchableOpacity } from 'react-native';
+import { API, graphqlOperation, Auth } from 'aws-amplify';
+import { createGroup, createUserGroup } from '../../src/graphql/mutations';
+import { getUser } from '../../src/graphql/queries';
 import colors from '../config/colors';
+import { useNavigation } from '@react-navigation/native';
 
 export default function CreateGroupScreen() {
+  const [groupName, setGroupName] = useState('');
+  const [groupKey, setGroupKey] = useState('');
+  const [groupId, setGroupId] = useState(null); // initialize group id to null
+  const [user, setUser] = useState(null);
+  const navigation = useNavigation();
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const authUser = await Auth.currentAuthenticatedUser({
+          bypassCache: true,
+        });
+        const userData2 = await API.graphql(
+            graphqlOperation(getUser, { id: authUser.attributes.sub })
+          );
+        console.log(authUser.attributes.sub);
+        setUser(authUser.attributes.sub);
+      } catch (error) {
+        console.log('Error fetching user data:', error);
+      }
+    }; 
+    fetchUser();
+  }, []);
 
-    const navigation = useNavigation();
-    const handleGoBack = () => {
-        navigation.popToTop();
+  const handleGoBack = () => {
+    navigation.popToTop();
+  }
+  const handleCreateGroup = async () => {
+    try {
+      const input = {
+        name: groupName,
+        groupKey: groupKey,
+        // add other fields as necessary
+      };
+      const result = await API.graphql(graphqlOperation(createGroup, { input }));
+      const createdGroup = result.data.createGroup;
+      setGroupId(createdGroup.id); // set group id after creating group
+  
+      // create user group
+      const userGroupInput = {
+        groupId: createdGroup.id,
+        userId: user,
+        // add other fields as necessary
+      };
+      const userGroupResult = await API.graphql(
+        graphqlOperation(createUserGroup, { input: userGroupInput })
+      );
+      const createdUserGroup = userGroupResult.data.createUserGroup;
+      console.log('Created user group:', createdUserGroup);
+      navigation.pop();
+    } catch (error) {
+      console.log('Error creating group', error);
     }
+  };
+  
 
-    return (
-        <SafeAreaView style={styles.body}>
 
-            <TextInput 
-                style={styles.input}
-                placeholder= 'Group Name'
-                onChangeText={(value) => setTitle(value)}
-            />
 
-            <View style={styles.buttonContainer}>
-                <TouchableOpacity style={styles.backButton} onPress={handleGoBack}>
-                    <Text style={styles.buttonText}>GO BACK</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.confirmButton} onPress={handleGoBack}>
-                    <Text style={styles.buttonText}>Confirm</Text>
-                </TouchableOpacity>
-            </View>
-            
+  return (
+    <SafeAreaView style={styles.body}>
+    <View style={styles.inputContainer}>
+        <TextInput
+        style={styles.input}
+        placeholder="Group Name"
+        onChangeText={(value) => setGroupName(value)}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Group Key"
+        onChangeText={(value) => setGroupKey(value)}
+      />
+    </View>
 
-        </SafeAreaView>
-    )
+
+
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.backButton} onPress={handleGoBack}>
+          <Text style={styles.buttonText}>GO BACK</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.confirmButton}
+          onPress={handleCreateGroup}
+        >
+          <Text style={styles.buttonText}>Confirm</Text>
+        </TouchableOpacity>
+      </View>
+
+    </SafeAreaView>
+  );
 }
+
 
 const styles = StyleSheet.create({
     body: {
         flex: 1,
-        alignItems: 'center',
         padding: 10,
         justifyContent: 'center',
         backgroundColor: colors.background,
     },
     input: {
         borderWidth: 1,
-        borderColor: '#C0C0C0',
+        borderColor: colors.primary,
         borderRadius: 10,
         backgroundColor: '#fff',
         textAlign: 'left',
@@ -54,12 +115,19 @@ const styles = StyleSheet.create({
         width: '100%',
         margin: 10,
     },
+    inputContainer: {
+        alignItems: 'center',
+        flex: 1,
+        justifyContent: 'center',
+        padding: 5,
+      },
     buttonContainer: {
         flexDirection: 'row',
         justifyContent: 'space-between',
+        padding: 5,
       },
     backButton: {
-        backgroundColor: '#000',
+        backgroundColor: colors.primary,
         borderRadius: 10,
         padding: 10,
         alignItems: 'center',
@@ -67,7 +135,7 @@ const styles = StyleSheet.create({
         width: '48%',
       },
       confirmButton: {
-        backgroundColor: '#000',
+        backgroundColor: colors.primary,
         borderRadius: 10,
         padding: 10,
         alignItems: 'center',
@@ -76,5 +144,6 @@ const styles = StyleSheet.create({
       },
     buttonText: {
         color: '#fff',
+        fontWeight: 'bold',
     }
 })
--- moneysocial/app/screens/groupList.js
+++ moneysocial/app/screens/groupList.js
@@ -1,64 +1,141 @@
-import React, { useRef } from 'react';
-import { Animated, FlatList, StyleSheet, Text, Touchable, TouchableOpacity, View } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { useNavigation } from '@react-navigation/native';
+import React, { useState, useEffect } from 'react';
+import { Animated, FlatList, StyleSheet, Text, Touchable, TouchableOpacity, View, ActivityIndicator } from 'react-native';
+import { API, graphqlOperation, Auth } from 'aws-amplify';
+import { createGroup, createUserGroup } from '../../src/graphql/mutations';
+import { getGroup, listUserGroups, userGroupsByUserId } from '../../src/graphql/queries';
+import { useNavigation, useFocusEffect } from '@react-navigation/native';
 import colors from '../config/colors';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
-const GroupData = [
-  { id: 'group1', name: 'Group 1', value: 100 },
-  { id: 'group2', name: 'Group 2', value: 200 },
-];
 
-export default function MyComponent() {
+export default function GroupList() {
 
   const navigation = useNavigation();
+
+
   const handleGroupScreen = () => {
     navigation.navigate("CreateGroupScreen");
   }
-  const renderItem = ({ item }) => (
-    <TouchableOpacity style={styles.displayBalance}>
-      <Text style={styles.displayText}>{`${item.name}: $${item.value}`}</Text>
-    </TouchableOpacity>
+
+  const [user, setUser] = useState(null);
+  const [groups, setGroups] = useState(null);
+  const [userGroups, setUserGroups] = useState([]);
+  const [loading, setLoading] = useState(true);
+  
+  useFocusEffect(
+    React.useCallback(() => {
+      fetchUserGroups();
+    }, [])
   );
+  
+    const fetchUserGroups = async () => {
+      try {
+        const userInfo = await Auth.currentAuthenticatedUser();
+        const userId = userInfo.attributes.sub;
+        const result = await API.graphql(graphqlOperation(userGroupsByUserId, { userId }));
+        const userGroupsData = result.data.userGroupsByUserId.items;
+        setUserGroups(userGroupsData);
+        const groupIds = userGroupsData.map(group => group.groupId);
+        console.log(groupIds);
+        const groups = [];
+        for (const groupId of groupIds) {
+          const group = await API.graphql(graphqlOperation(getGroup, { id: groupId }));
+          groups.push(group.data.getGroup);
+        }
+        setGroups(groups);
+        console.log(groups);
+        setLoading(false);
+      } catch (error) {
+        console.log('Error fetching user groups', error);
+      }
+    };
+  
+    const renderGroup = ({ item }) => {
+      return (
+        <TouchableOpacity style={styles.groupItem}>
+          <Text style={styles.groupName}>{item.name}</Text>
+          <Text style={styles.keyText}>Key: {item.groupKey}</Text>
+          <Text style={styles.idText}>Group ID: {item.id}</Text>
+        </TouchableOpacity>
+      );
+    };
+    
+  
+    if (loading) {
+      return <Text>Loading...</Text>;
+    }
+  
+    return (
+      <SafeAreaView style={styles.container}>
+          <FlatList
+          data={groups}
+          renderItem={renderGroup}
+          keyExtractor={item => item.id}
+          contentContainerStyle={{ flexGrow: 1 }}
+          ListEmptyComponent={<Text>No user groups found</Text>}
+          style={styles.groupListContainer}
+          />
 
-  return (
-    <SafeAreaView style={styles.container}>
-      <Text style={styles.label}>Groups List:</Text>
-      <FlatList
-        data={GroupData}
-        renderItem={renderItem}
-        keyExtractor={(item) => item.id}
-      />
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.createButton} onPress={handleGroupScreen}>
+            <Text style={styles.createText}>Create Group</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.createButton} onPress={handleGroupScreen}>
+            <Text style={styles.createText}>Create Group</Text>
+          </TouchableOpacity>
+        </View>
+        
+      </SafeAreaView>
+      
+    );
+  };
 
-
-
-      <TouchableOpacity onPress={handleGroupScreen}>
-        <Text>ADD GROUP</Text>
-      </TouchableOpacity>
-    </SafeAreaView>
-  );
-}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: colors.background,
   },
-  displayBalance: {
-    padding: 20,
-    borderBottomWidth: 1,
-    borderBottomColor: colors.accent,
+  groupListContainer: {
+    paddingVertical: 15,
+    paddingHorizontal: 8,
   },
-  displayText: {
-    fontSize: 18,
-    fontWeight: 'bold',
+  groupItem: {
+    justifyContent: 'space-between',
+    marginVertical: 8,
+    padding: 10,
+    backgroundColor: colors.primary,
+    borderRadius: 10,
   },
-  label: {
-    fontSize: 24,
+  groupName: {
     fontWeight: 'bold',
-    marginVertical: 10,
-    marginHorizontal: 20,
-    paddingTop: 20,
-    paddingBottom: 10
+    fontSize: 16,
+    color: '#fff',
+  },
+  keyText: {
+    color:'#fff',
+    fontSize: 14,
+  },
+  idText: {
+    fontSize: 10,
+    color: '#fff',
+  },
+  createText: {
+    fontWeight: 'bold',
+    fontSize: 16,
+    color: '#fff',
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 5,
+  },
+  createButton: {
+    backgroundColor: colors.primary,
+    borderRadius: 10,
+    padding: 10,
+    alignItems: 'center',
+    marginBottom: 10,
+    width: '49%',
   },
 });
